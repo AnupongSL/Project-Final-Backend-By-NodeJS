9b62de9cdbbfcb92e988e83021dfeb09185a2f64
Apisit Janpalee <aps.axm@gmail.com>
2023-05-19 17:19:39 +0700

    post

--- src/routers/manager.route.js
+++ src/routers/manager.route.js
@@ -4,7 +4,7 @@ const jwt = require('../middleware/jwt')
 
 router.get('/getmanager', managerController.getManager) //ดึงข้อมูลเจ้าของร้านทั้งหมด
 router.get('/getmanager/:id', managerController.getManagerByID) //ดึงข้อมูลเจ้าของร้านตามไอดีของเจ้าของร้าน
-router.get('/getmanagerbyusername', managerController.getManagerUsername) // ดึงข้อมูลเจ้าของร้านตาม username
+router.post('/getmanagerbyusername', managerController.getManagerUsername) // ดึงข้อมูลเจ้าของร้านตาม username
 router.get('/getmanagerbyusernameemail', managerController.getManagerUsernameEmail) //ดึงข้อมูลเจ้าของร้านตาม username และ email
 router.get('/checktoken', jwt.verifyToken, managerController.checkToken); // เช็คว่าล้อคอินไหม แล้วเช็คว่าคนที่ล้อคอินคือใคร owner, manager, admin
 // router.get('/forget-password', managerController.forgetPasswordManager)
